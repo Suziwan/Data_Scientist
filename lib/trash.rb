f4d027be609de7d17815e083bd4a710c48a7406f
Suziwan <christe.suzanne@gmail.com>
2023-01-19 18:10:15 +0000

    Third Commit

---
+++ lib/trash.rb
@@ -0,0 +1,17 @@
+# Ruby code for length() method
+  
+# declaring array
+a = ["abc", "nil", "dog"]
+  
+# declaring array
+b = ["cat", nil]
+  
+# declaring array
+c = ["cow", nil, "dog"]
+  
+# length method example
+puts "length() method form : #{a.length()}\n\n"
+  
+puts "length() method form : #{b.length()}\n\n"
+  
+puts "length() method form : #{c.length()}\n\n"
